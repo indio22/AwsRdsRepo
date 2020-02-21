74f475a7820606fc0f5e0a832ea6f1c8ef550e7c
Czajka <timothy.r.czajka@accenture.com>
2020-02-20 23:59:46 -0600

    Changes to the handler

--- handler.js
+++ handler.js
@@ -1,18 +1,35 @@
 'use strict';
 
-module.exports.hello = async event => {
-  return {
-    statusCode: 200,
-    body: JSON.stringify(
-      {
-        message: 'Go Serverless v1.0! Your function executed successfully!',
-        input: event,
-      },
-      null,
-      2
-    ),
-  };
+// module.exports.hello = async event => {
+//   return {
+//     statusCode: 200,
+//     body: JSON.stringify(
+//       {
+//         message: 'Go Serverless v1.0! Your function executed successfully!',
+//         input: event,
+//       },
+//       null,
+//       2
+//     ),
+//   };
+
+// event is populated by the event that triggers the object.  In this case from the yml file, an http event
+// so it will have body and other stuff as an http even has?
+module.exports = {
+  create: async(event, context) => {
+    let bodyObj = {};
+    try {
+      bodyObj = JSON.parse(event.body)
+    }
+    catch (jsonerror) {
+        console.log('Hey there was an error: ', jsonerror)
+        return {
+          statusCode: 400
+        }
+    }
+  }
+};
 
   // Use this code if you don't use the http event with the LAMBDA-PROXY integration
   // return { message: 'Go Serverless v1.0! Your function executed successfully!', event };
-};
+
