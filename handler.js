4178fe02d8f7ed61287b0a29ada698607ff0fc6d
Czajka <timothy.r.czajka@accenture.com>
2020-03-03 16:03:39 -0600

    Added Get to handler and front end

--- handler.js
+++ handler.js
@@ -16,7 +16,7 @@ const AWS = require('aws-sdk');
 // event is populated by the event that triggers the object.  In this case from the yml file, an http event
 // so it will have body and other stuff as an http even has?
 module.exports = {
-  create: async(event, context) => {
+  create: async (event, context) => {
     // This will hold the body of the event.
     console.log('Entering the create function.')
     let bodyObj = {};
@@ -25,10 +25,10 @@ module.exports = {
     }
     // Maybe the event info was wrong.
     catch (jsonerror) {
-        console.log('Event message sent to lambda was unacceptable, might not have been http event ', jsonerror)
-        return {
-          statusCode: 400
-        }
+      console.log('Event message sent to lambda was unacceptable, might not have been http event ', jsonerror)
+      return {
+        statusCode: 400
+      }
     }
 
     // Maybe there was no name param or it was empty?
@@ -47,7 +47,7 @@ module.exports = {
       Item: {
         name: bodyObj.name,
         make: bodyObj.make,
-        type: bodyObj.type        
+        type: bodyObj.type
       }
     }
 
@@ -59,7 +59,7 @@ module.exports = {
       putResult = await rdsdb.put(putParams).promise();
     }
     // In case there was an error executing the put.
-    catch(putError) {
+    catch (putError) {
       console.log('There was an error putting the transmission data to the DB.');
       console.log('Here are the parameters that were sent to the DB: ', putParams);
       return {
@@ -71,7 +71,45 @@ module.exports = {
     return {
       statusCode: 201
     }
-  }
+  },
+
+  list: async (event, context) => {
+    console.log('Entering the returnTransmission function.')
+    let scanParams = {
+      TableName: process.env.DB_TRANSMISSION_TABLE
+    }
+
+    let scanResult = {}
+    try {
+      let rdsdb = new AWS.DynamoDB.DocumentClient();
+      scanResult = await rdsdb.scan(scanParams).promise();
+    }
+    catch (scanError) {
+      console.log('There was an error scanning the transmission table.');
+      console.log('Here are the scan parameters that were sent to the DB: ', scanParams);
+      return {
+        statusCode: 500
+      }
+    }
+
+    if ((scanResult.Items == null) ||
+      !Array.isArray(scanResult) ||
+      scanResult.Items.length == 0) {
+      return {
+        statusCode: 404
+      }
+    }
+
+    return {
+      statusCode: 200,
+      body: JSON.stringify(scanResult.Items.map(transmission ==> {
+        return {
+          name: transmission.name,
+          make: transmission.make
+        }
+      }))
+    }
+  };
 };
 
   // Use this code if you don't use the http event with the LAMBDA-PROXY integration
